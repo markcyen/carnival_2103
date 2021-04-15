dc0467b1686d91a17bc0ed59b199533b443f09d0
MarkY <markcyen@yahoo.com>
2021-04-15 09:39:37 -0600

    Implement recommend_rides method

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -10,4 +10,12 @@ class Carnival
   def add_ride(ride)
     @rides << ride
   end
+
+  def recommend_rides(attendee)
+    capture_rides = []
+    @rides.each do |ride|
+      capture_rides << ride if attendee.interests.include?(ride.name)
+    end
+    capture_rides
+  end
 end
