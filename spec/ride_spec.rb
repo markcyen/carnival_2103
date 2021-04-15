27b8f2231a364d10f3dfa229b8cde8261c2acf09
MarkY <markcyen@yahoo.com>
2021-04-15 09:07:16 -0600

    Create addition initialize tests

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -3,9 +3,18 @@ require './lib/ride'
 
 RSpec.describe Ride do
   context '#initialize' do
+    ride = Ride.new({name: 'Ferris Wheel', cost: 0})
+
     it 'exists' do
-      ride = Ride.new({name: 'Ferris Wheel', cost: 0})
       expect(ride).to be_instance_of(Ride)
+    end
+
+    it 'has a name' do
+      expect(ride.name).to eq('Ferris Wheel')
+    end
+
+    it 'has a cost' do
+      expect(ride.cost).to eq(0)
     end
   end
 end
