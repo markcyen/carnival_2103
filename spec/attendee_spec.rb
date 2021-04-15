6e8491866e1d3608b9a9461d8626ca358a858f36
MarkY <markcyen@yahoo.com>
2021-04-15 09:16:13 -0600

    Create add_interest and interests tests

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -16,5 +16,20 @@ RSpec.describe Attendee do
     it 'has spending money' do
       expect(attendee.spending_money).to eq(20)
     end
+
+    it 'has no interests yet' do
+      expect(attendee.interests).to eq([])
+    end
+  end
+
+  context '#add_interest' do
+    attendee = Attendee.new('Bob', 20)
+
+    it 'adds interests' do
+      attendee.add_interest('Bumper Cars')
+      attendee.add_interest('Ferris Wheel')
+
+      expect(attendee.interests).to eq(["Bumper Cars", "Ferris Wheel"])
+    end
   end
 end
